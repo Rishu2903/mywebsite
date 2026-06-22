7e626b360cee883297106c310b089317e5e15a20
Rishabh Kataria <rishabhkataria.work@gmail.com>
2026-06-22 16:59:28 +0530

    update2

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,71 +1,80 @@
-  import { NavLink } from "react-router-dom";
-  import "../styles/navbar.css";
+import { NavLink } from "react-router-dom";
+import "../styles/navbar.css";
 
-  export default function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg custom-navbar sticky-top">
-        <div className="container">
+export default function Navbar() {
+  const closeNavbar = () => {
+    const toggleBtn = document.querySelector(".navbar-toggler");
+    const nav = document.getElementById("nav");
 
-          {/* Brand */}
-          <NavLink className="navbar-brand brand fw-bold" to="/">
-            JSRAK
-          </NavLink>
+    if (nav.classList.contains("show")) {
+      toggleBtn.click();
+    }
+  };
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar sticky-top">
+      <div className="container">
+        {/* Brand */}
+        <NavLink className="navbar-brand brand fw-bold" to="/">
+          JSRAK
+        </NavLink>
 
-          {/* Toggle */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#nav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        {/* Toggle */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#nav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          {/* Links */}
-          <div className="collapse navbar-collapse" id="nav">
-            <ul className="navbar-nav ms-auto gap-3">
+        {/* Links */}
+        <div className="collapse navbar-collapse" id="nav">
+          <ul className="navbar-nav ms-auto gap-3">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                onClick={closeNavbar}
+                className={({ isActive }) =>
+                  isActive ? "nav-link active-link" : "nav-link"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
 
-              <li className="nav-item">
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active-link" : "nav-link"
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
+            <li className="nav-item">
+              <NavLink
+                to="/services"
+                onClick={closeNavbar}
+                className={({ isActive }) =>
+                  isActive ? "nav-link active-link" : "nav-link"
+                }
+              >
+                Services
+              </NavLink>
+            </li>
 
-              <li className="nav-item">
-                <NavLink to="/services" className={({ isActive }) =>
-                    isActive ? "nav-link active-link" : "nav-link"
-                  }>
-                  Services
-                </NavLink>
-              </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
 
-              <li className="nav-item">
-                <NavLink to="/about" className="nav-link">
-                  About
-                </NavLink>
-              </li>
+            <li className="nav-item">
+              <NavLink to="/portfolio" className="nav-link">
+                Portfolio
+              </NavLink>
+            </li>
 
-              <li className="nav-item">
-                <NavLink to="/portfolio" className="nav-link">
-                  Portfolio
-                </NavLink>
-              </li>
-
-              <li className="nav-item">
-                <NavLink to="/contact" className="nav-link">
-                  Contact
-                </NavLink>
-              </li>
-
-            </ul>
-          </div>
-
+            <li className="nav-item">
+              <NavLink to="/contact" className="nav-link">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
